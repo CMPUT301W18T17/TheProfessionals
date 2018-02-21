0dcc3852135af11b10245882ae18c9a9ed1b92e5
sewingpillows <allison.boukall@gmail.com>
2018-02-20 20:59:33 -0700

    Added Review class

---
+++ app/src/main/java/professional/team17/com/professional/Review.java
@@ -0,0 +1,68 @@
+package professional.team17.com.professional;
+
+/**
+ * Created by ag on 2018-02-20.
+ */
+
+public class Review {
+
+    private Double score;
+    private String comment;
+    private String profileName; //person who gave review
+    private String reviewTitle;
+
+    public Review(Double score, String reviewer, String comment, String title){
+        this.score = score;
+        this.profileName  = reviewer;
+        this.comment = comment;
+        this.reviewTitle = title;
+
+    }
+
+    public Review(Double score, String reviewer, String title){
+        this.score = score;
+        this.profileName  = reviewer;
+        this.reviewTitle = title;
+    }
+    /* GETTERS */
+
+    public Double getScore(){
+        return this.score;
+    }
+
+    public String getComment(){
+        return this.comment;
+    }
+
+    public String getProfileName(){
+        return this.profileName;
+    }
+
+    public String getReviewTitle(){
+        return this.reviewTitle;
+    }
+
+    /*SETTERS*/
+    public void setScore(Double score){
+        this.score = score;
+    }
+
+
+    public void setComment(String comment){
+        this.comment = comment;
+
+    }
+
+    private void setProfileName(String name){
+        this.profileName = name;
+    }
+
+    public void setReviewTitle(String title){
+        this.reviewTitle = title;
+    }
+
+
+
+
+
+}
