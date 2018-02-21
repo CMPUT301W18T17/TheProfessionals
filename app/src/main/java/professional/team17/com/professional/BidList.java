aa2e254e7958d54437ec36df2bb171148050f5da
Hailan Xu <hailan@ualberta.ca>
2018-02-20 19:20:19 -0700

    BidListTest added

---
+++ app/src/main/java/professional/team17/com/professional/BidList.java
@@ -0,0 +1,31 @@
+package professional.team17.com.professional;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Hailan on 2018-02-20.
+ */
+
+public class BidList {
+    private ArrayList<Bid> bids = new ArrayList<Bid>();
+
+    public BidList(){
+        // Nothing
+    }
+
+    public Bid getBid(int index){
+        return bids.get(index);
+    }
+
+    public boolean hasBid(Bid bid){
+        return bids.contains(bid);
+    }
+
+    public void add(Bid bid){
+        bids.add(bid);
+    }
+
+    public void delete(Bid bid){
+        bids.remove(bid);
+    }
+}
