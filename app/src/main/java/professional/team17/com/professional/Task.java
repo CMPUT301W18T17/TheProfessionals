20ba65fb82e43ce9d5f00d0c526a8153591df000
Lauren Hubert-Leblanc <hubertle@ualberta.ca>
2018-02-22 19:59:56 -0700

    Task Test Cases added

--- app/src/main/java/professional/team17/com/professional/Task.java
+++ app/src/main/java/professional/team17/com/professional/Task.java
@@ -1,38 +1,45 @@
 package professional.team17.com.professional;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Task {
-    private String taskRequester; //person who requested the task
-    private String title;
+    private String profileName;     /* person who requested the task */
+    private String name;
     private String description;
-    private String Location;
+    private String location;
     private Date date;
-    //private String profilePic;
+    private String uniqueID;
+    private ArrayList<String> photos;
+    private ArrayList<Bid> bids;
+    private String status;
 
 
-    public Task(String taskRequester, String title, String description, String location) {
-        this.taskRequester = taskRequester;
-        this.title = title;
+
+    public Task(String profileName, String name, String description, String location, Date date, String ID) {
+        this.profileName = profileName;
+        this.name = name;
         this.description = description;
-        Location = location;
-        this.date = new Date();
+        this.location = location;
+        this.date = date;
+        this.uniqueID = ID;
+        this.status = "Requested";
     }
 
-    public String getTaskRequester() {
-        return taskRequester;
+    public String getProfileName() {
+        return profileName;
     }
 
-    public void setTaskRequester(String taskRequester) {
-        this.taskRequester = taskRequester;
+    public void setProfileName(String profileName) {
+        this.profileName = profileName;
     }
 
-    public String getTitle() {
-        return title;
+    public String getName() {
+        return name;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getDescription() {
@@ -44,14 +51,74 @@ public class Task {
     }
 
     public String getLocation() {
-        return Location;
+        return location;
     }
 
     public void setLocation(String location) {
-        Location = location;
+        this.location = location;
     }
 
     public Date getDate() {
         return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getUniqueID() {
+        return uniqueID;
+    }
+
+    public void setUniqueID(String ID) {
+        this.uniqueID = ID;
+    }
+
+    public ArrayList<String> getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(ArrayList<String> photos) {
+        this.photos = photos;
+    }
+
+    public void addPhoto(String photo){
+        this.photos.add(photo);
+    }
+
+    public void removePhoto(String photo){
+        this.photos.remove(photo);
+    }
+
+    public ArrayList<Bid> getBids() {
+        return bids;
+    }
+
+    public void setBids(ArrayList<Bid> bids) {
+        this.bids = bids;
+    }
+
+    public void addBid(Bid bid){
+        this.bids.add(bid);
+    }
+
+    public void removeBid(Bid bid){
+        this.bids.remove(bid);
+    }
+
+    public void clearBids(){
+        this.bids.clear();
+    }
+
+    public int getLowestBid(){
+        /* implement this later */
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 }
---
+++ app/src/test/java/professional/team17/com/professional/TaskTest.java
@@ -0,0 +1,185 @@
+package professional.team17.com.professional;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * Created by Lauren
+ * on 2018-02-22.
+ */
+
+public class TaskTest extends ActivityInstrumentationTestCase2 {
+
+    private String profileName = "Testing";
+    private String name = "Tast Title";
+    private String description = "Task Description";
+    private String location = "Task Location";
+    private Date date = new Date();
+    private String uniqueID = "Task ID";
+    private ArrayList<String> photos = new ArrayList<>();
+    private ArrayList<Bid> bids = new ArrayList<>();
+    private String status = "Task Status";
+
+    public TaskTest(){
+        super(Task.class);
+    }
+
+    public void getProfileNameTest() {
+        Task task = new Task(profileName, name, description, location, date, uniqueID);
+        assertEquals(profileName, task.getProfileName());
+    }
+
+    public void setProfileNameTest() {
+        Task task = new Task(profileName, name, description, location, date, uniqueID);
+        this.profileName = "Task Requester";
+        task.setProfileName(this.profileName);
+        assertEquals(profileName, task.getProfileName());
+    }
+
+    public void getNameTest() {
+        Task task = new Task(profileName, name, description, location, date, uniqueID);
+        assertEquals(name, task.getName());
+    }
+
+    public void setNameTest() {
+        Task task = new Task(profileName, name, description, location, date, uniqueID);
+        this.name = "Testing";
+        task.setName(name);
+        assertEquals(name, task.getName());
+    }
+
+    public void getDescriptionTest() {
+        Task task = new Task(profileName, name, description, location, date, uniqueID);
+        assertEquals(description, task.getDescription());
+    }
+
+    public void setDescriptionTest() {
+        Task task = new Task(profileName, name, description, location, date, uniqueID);
+        this.description = "Testing";
+        task.setDescription(description);
+        assertEquals(description, task.getDescription());
+    }
+
+    public void getLocationTest() {
+        Task task = new Task(profileName, name, description, location, date, uniqueID);
+        assertEquals(location, task.getLocation());
+    }
+
+    public void setLocationTest() {
+        Task task = new Task(profileName, name, description, location, date, uniqueID);
+        this.location = "Testing";
+        task.setLocation(location);
+        assertEquals(location, task.getLocation());
+    }
+
+    public void getDateTest() {
+        Task task = new Task(profileName, name, description, location, date, uniqueID);
+        assertEquals(date, task.getDate());
+    }
+
+    public void setDateTest() {
+        Task task = new Task(profileName, name, description, location, date, uniqueID);
+        this.date = new Date();
+        task.setDate(date);
+        assertEquals(date, task.getDate());
+    }
+
+    public void getUniqueIDTest() {
+        Task task = new Task(profileName, name, description, location, date, uniqueID);
+        assertEquals(uniqueID, task.getUniqueID());
+    }
+
+    public void setUniqueIDTest() {
+        Task task = new Task(profileName, name, description, location, date, uniqueID);
+        this.uniqueID = "Testing";
+        task.setUniqueID(uniqueID);
+        assertEquals(uniqueID, task.getUniqueID());
+    }
+
+    public void getPhotosTest() {
+        Task task = new Task(profileName, name, description, location, date, uniqueID);
+        assertEquals(photos, task.getPhotos());
+    }
+
+    public void setPhotosTest() {
+        Task task = new Task(profileName, name, description, location, date, uniqueID);
+        assertEquals(photos, task.getPhotos());
+    }
+
+    public void addPhotoTest(){
+        Task task = new Task(profileName, name, description, location, date, uniqueID);
+        task.addPhoto("test");
+        assertEquals(photos, task.getPhotos());
+    }
+
+    public void removePhotoTest(){
+        Task task = new Task(profileName, name, description, location, date, uniqueID);
+        task.addPhoto("test");
+        task.addPhoto("test2");
+        task.removePhoto("test");
+        assertEquals(photos, task.getPhotos());
+    }
+
+    public void getBidsTest() {
+        Task task = new Task(profileName, name, description, location, date, uniqueID);
+        assertEquals(bids, task.getBids());
+    }
+
+    public void setBidsTest() {
+        Task task = new Task(profileName, name, description, location, date, uniqueID);
+        assertEquals(bids, task.getBids());
+    }
+
+    public void addBidTest(){
+        Task task = new Task(profileName, name, description, location, date, uniqueID);
+        Bid bid = new Bid("name", 0);
+        task.addBid(bid);
+        assertEquals(bids, task.getBids());
+    }
+
+    public void removeBidTest(){
+        Task task = new Task(profileName, name, description, location, date, uniqueID);
+        Bid bid = new Bid("name", 0);
+        task.addBid(bid);
+        Bid bid2 = new Bid("name2", 1);
+        task.addBid(bid2);
+        task.removeBid(bid);
+        assertEquals(bids, task.getBids());
+    }
+
+    public void clearBidsTest(){
+        Task task = new Task(profileName, name, description, location, date, uniqueID);
+        Bid bid = new Bid("name", 0);
+        task.addBid(bid);
+        Bid bid2 = new Bid("name2", 1);
+        task.addBid(bid2);
+        task.clearBids();
+        assertEquals(bids, task.getBids());
+    }
+
+    public void getLowestBidTest(){
+        Task task = new Task(profileName, name, description, location, date, uniqueID);
+        Bid bid = new Bid("name", 0);
+        task.addBid(bid);
+        Bid bid2 = new Bid("name2", 1);
+        task.addBid(bid2);
+        int lowestBid = task.getLowestBid();
+        assertEquals(lowestBid, task.getLowestBid());
+    }
+
+    public void getStatusTest() {
+        Task task = new Task(profileName, name, description, location, date, uniqueID);;
+        assertEquals(status, task.getStatus());
+    }
+
+    public void setStatusTest() {
+        Task task = new Task(profileName, name, description, location, date, uniqueID);
+        this.status = "Testing";
+        task.setStatus(status);
+        assertEquals(status, task.getStatus());
+    }
+}
+
+
