46b55b3a73c11296edc6721068dc8287c4056b4d
sewingpillows <allison.boukall@gmail.com>
2018-03-02 23:00:20 -0700

    remove focus

--- app/src/main/java/professional/team17/com/professional/Search_Activity.java
+++ app/src/main/java/professional/team17/com/professional/Search_Activity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -56,7 +57,7 @@ public class Search_Activity extends AppCompatActivity {
                 Toast toast = Toast.makeText(context, "SEARCH ENTERED"+query, duration);
                 toast.show();
                 displayResults();
-
+                searchView.clearFocus(); //remove focus on submit
                 return false;
             }
             @Override
