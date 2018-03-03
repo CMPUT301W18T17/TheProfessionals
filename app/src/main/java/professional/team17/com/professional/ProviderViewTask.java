a070f2ef4fa4584a4d54ff84269087bd214a3d2b
sewingpillows <allison.boukall@gmail.com>
2018-03-02 23:01:11 -0700

    providertask

---
+++ app/src/main/java/professional/team17/com/professional/ProviderViewTask.java
@@ -0,0 +1,13 @@
+package professional.team17.com.professional;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ProviderViewTask extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_provider_view_task);
+    }
+}
