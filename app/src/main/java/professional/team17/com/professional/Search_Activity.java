456d7a9aab7e90f43496619ac3c40ce92a8ed3e9
sewingpillows <allison.boukall@gmail.com>
2018-03-02 22:42:53 -0700

    add in search

--- app/src/main/java/professional/team17/com/professional/Search_Activity.java
+++ app/src/main/java/professional/team17/com/professional/Search_Activity.java
@@ -1,5 +1,6 @@
 package professional.team17.com.professional;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,13 +8,13 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SearchView;
-
-import java.util.Date;
+import android.widget.Toast;
 
 public class Search_Activity extends AppCompatActivity {
     private ArrayAdapterSearchResults searchAdapterHelper;
     private ListView listView;
     private SearchView searchView;
+    private TaskList taskList;
 
     //TODO DELETE
     private TaskList dummyTaskList;
@@ -29,35 +30,34 @@ public class Search_Activity extends AppCompatActivity {
         dummyTaskList.addTask(task3);
     }
 
-    //TODO this will fill results from search (may need to take parameters in bundle)
-    public void getTasks() {
-        dummyDate();
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-        getTasks();
-        searchAdapterHelper = new ArrayAdapterSearchResults(this, dummyTaskList);
+        taskList = new TaskList();
+        searchAdapterHelper = new ArrayAdapterSearchResults(this, taskList);
         listView =findViewById(R.id.Search_Activity_list);
         listView.setAdapter(searchAdapterHelper);
         listView.setOnItemClickListener(clickListener);
+
+
         searchView = (SearchView) findViewById(R.id.Search_Activity_Input);
         searchView.setQueryHint("Enter search");
+
 
 
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-                //SearchAdapter mSearchAdapter = new SearchAdapter(MainActivity.this, dictionaryObject);
-                //listView.setAdapter(mSearchAdapter);
-                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                    }
-                });
-                return true;
+
+                Context context = getApplicationContext();
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, "SEARCH ENTERED"+query, duration);
+                toast.show();
+                displayResults();
+
+                return false;
             }
             @Override
             public boolean onQueryTextChange(String newText) {
@@ -82,4 +82,20 @@ public class Search_Activity extends AppCompatActivity {
         }
 
     };
+
+
+    //TODO this will fill results from search (may need to take parameters in bundle)
+    public TaskList getTasks() {
+        dummyDate();
+        return dummyTaskList;
+    }
+
+    //This updates the adapter with the results
+    public void displayResults(){
+        taskList.clear();
+        taskList.addAll(getTasks());
+        searchAdapterHelper.notifyDataSetChanged();
+    }
+
+
 }
