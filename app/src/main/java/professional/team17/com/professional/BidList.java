f0f258c62051916b0e82aa19ec705bd00f114217
sewingpillows <allison.boukall@gmail.com>
2018-03-03 22:02:45 -0700

    update bidlist for taskview

--- app/src/main/java/professional/team17/com/professional/BidList.java
+++ app/src/main/java/professional/team17/com/professional/BidList.java
@@ -3,9 +3,12 @@ package professional.team17.com.professional;
 import java.util.ArrayList;
 
 public class BidList {
-    private ArrayList<Bid> bids = new ArrayList<Bid>();
+    private ArrayList<Bid> bids;
+    private Bid lowest;
 
     public BidList(){
+        lowest = null;
+        bids = new ArrayList<Bid>();
         // Nothing
     }
 
@@ -17,23 +20,48 @@ public class BidList {
         return bids.contains(bid);
     }
 
+    //add bid and keep track of lowest
     public void add(Bid bid){
+        if (bids.isEmpty()){
+            lowest = bid;
+        }
+        else if (bid.getAmount()<lowest.getAmount()) {
+            lowest =  bid;
+        }
         bids.add(bid);
     }
 
     public void delete(Bid bid){
         bids.remove(bid);
+        if (bid==lowest){
+            lowest = getLowest();
+        }
     }
 
     //remove all bids bud the one plugged in
     public void deleteAllExcept(Bid bid){
 
+
     }
 
-    public Bid getLowest(){
-        //TODO
-        return null;
+    /**
+     *
+     * @return the bid with lowest bid.amount in the list
+     */
+    public Bid getLowest() {
+
+        if (bids.isEmpty()) {
+            return null;
+        }
+        Bid low = bids.get(0);
+        for (int i = 1; i < bids.size(); i++) {
+            if (bids.get(i).getAmount()<low.getAmount()){
+                low = bids.get(i);
+            }
+        }
+        return low;
     }
+
 
     public int getSize(){
         return bids.size();
