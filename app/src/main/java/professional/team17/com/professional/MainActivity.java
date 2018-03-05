74941e6f3e58896f2f9b201eaabbf5949f44f8e6
Unknown <ltyue@ualberta.ca>
2018-03-05 10:55:18 -0700

    Added Login navigation

--- app/src/main/java/professional/team17/com/professional/MainActivity.java
+++ app/src/main/java/professional/team17/com/professional/MainActivity.java
@@ -1,7 +1,10 @@
 package professional.team17.com.professional;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    //TODO: Login in navigation
+    public void logIn(View view){
+
+        //can change from ProviderViewTask, this is just a stand in
+        Intent intent = new Intent(this, ProviderViewTask.class);
+        EditText usernameBox = (EditText) findViewById(R.id.usernameBox);
+        String username = usernameBox.getText().toString();
+
+        //TODO Elastic search for username
+
+        //set usernameas global variable?
+
+        startActivity(intent);
+    }
+
+    public void signUp(View view) {
+
+        Intent intent = new Intent(this, SignUpActivity.class);
+
+        //not sure if I should be starting with result or not
+        startActivity(intent);
     }
 }
--- app/src/main/java/professional/team17/com/professional/SignUpActivity.java
+++ app/src/main/java/professional/team17/com/professional/SignUpActivity.java
@@ -9,5 +9,9 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //TODO: back to login navigation
+
+        //TODO Sign Up function including error handling
     }
 }
