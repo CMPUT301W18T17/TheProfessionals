536cb8d0ff1f51c8f95c25be2feebd35a24edd0a
sewingpillows <allison.boukall@gmail.com>
2018-03-04 23:01:19 -0700

    update taskrow and adapter to account for more bid views

--- app/src/main/java/professional/team17/com/professional/ArrayAdapterSearchResults.java
+++ app/src/main/java/professional/team17/com/professional/ArrayAdapterSearchResults.java
@@ -1,6 +1,7 @@
 package professional.team17.com.professional;
 
 import android.app.Activity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +9,8 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import java.util.List;
+
+import javax.security.auth.login.LoginException;
 
 /**
  * Created by ag on 2018-02-28.
@@ -27,7 +30,7 @@ public class ArrayAdapterSearchResults extends ArrayAdapter<Task> {
         // aided by https://github.com/codepath/android_guides/wiki/Using-an-ArrayAdapter-with-ListView
         public View getView(int position, View v, ViewGroup parent) {
             Task task= getItem(position);
-
+            Log.i("LOOOKINAT", "getView: "+position);
             if (v == null) {
                 v = LayoutInflater.from(getContext()).inflate(R.layout.search_task_row, parent, false);
             }
@@ -35,11 +38,31 @@ public class ArrayAdapterSearchResults extends ArrayAdapter<Task> {
             TextView statusTextField = (TextView) v.findViewById(R.id.search_task_row_statusText);
             TextView userNameTextField = (TextView) v.findViewById(R.id.search_task_row_userName);
             TextView taskTitleTextField = (TextView) v.findViewById(R.id.search_task_row_title);
+            TextView taskLowBidTextField = (TextView) v.findViewById(R.id.search_task_row_lowbid);
+            TextView taskMyBidTextField = (TextView) v.findViewById(R.id.search_task_row_mybid);
+            TextView taskLowBidAmountTextField = (TextView) v.findViewById(R.id.search_task_row_lowbidAmount);
+            TextView taskMyBidAmountTextField = (TextView) v.findViewById(R.id.search_task_row_mybidAmount);
+
 
             //plug in item to row
             statusTextField.setText(task.getStatus());
             userNameTextField.setText(task.getProfileName());
             taskTitleTextField.setText(task.getName());
+            Log.i("Task", "getView: "+task.isRequested()+task.getStatus()+task.getName());
+            if (task.isRequested()){
+                taskLowBidTextField.setVisibility(View.INVISIBLE);
+                taskMyBidTextField.setVisibility(View.INVISIBLE);
+                taskLowBidAmountTextField.setVisibility(View.INVISIBLE);
+                taskMyBidAmountTextField.setVisibility(View.INVISIBLE);
+            }
+            else {
+                taskLowBidAmountTextField.setText(task.getBids().getLowest().getAmountAsString());
+                taskLowBidTextField.setVisibility(View.VISIBLE);
+                taskMyBidTextField.setVisibility(View.VISIBLE);
+                taskLowBidAmountTextField.setVisibility(View.VISIBLE);
+                taskMyBidAmountTextField.setVisibility(View.VISIBLE);
+            }
+
 
             return v;
         }
