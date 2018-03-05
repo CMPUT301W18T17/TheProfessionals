66108bd92eb248f43babc16b339c278a955b574e
Unknown <ltyue@ualberta.ca>
2018-03-05 10:26:50 -0700

    Login and Sign up xml additions

---
+++ app/src/main/java/professional/team17/com/professional/SignUpActivity.java
@@ -0,0 +1,13 @@
+package professional.team17.com.professional;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
