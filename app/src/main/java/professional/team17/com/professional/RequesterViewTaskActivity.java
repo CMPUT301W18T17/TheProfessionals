668abd424520a11aff4f146eddfbe719c1f9218f
Lauren Hubert-Leblanc <hubertle@ualberta.ca>
2018-03-12 19:14:48 -0600

    Requested/Done buttons in RequesterViewTaskActivity implemented

--- app/src/main/java/professional/team17/com/professional/RequesterViewTaskActivity.java
+++ app/src/main/java/professional/team17/com/professional/RequesterViewTaskActivity.java
@@ -11,6 +11,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -26,7 +27,7 @@ import static android.view.View.VISIBLE;
 /**
  * The activity that is displayed when a user in the Requester role views one of their own tasks.
  */
-public class RequesterViewTaskActivity extends RequesterLayout {
+public class RequesterViewTaskActivity extends RequesterLayout implements ConfirmDialog.ConfirmDialogListener {
     /* Layout items */
     ImageButton backButton;
     ImageButton viewPhotos;
@@ -50,6 +51,7 @@ public class RequesterViewTaskActivity extends RequesterLayout {
     BidListAdapter bidAdapter;
     BidList bidList;
     Bid winningBid;
+    String dialogFlag;
 
     /**
      * On creation of the activity, set the layout elements, onClickListeners, and retrive
@@ -83,8 +85,6 @@ public class RequesterViewTaskActivity extends RequesterLayout {
         listView.setAdapter(bidAdapter);
 
 
-
-
         /* Set OnClickListeners */
         backButton.setOnClickListener(new ImageButton.OnClickListener() {
             @Override
@@ -110,14 +110,14 @@ public class RequesterViewTaskActivity extends RequesterLayout {
         requestedButton.setOnClickListener(new Button.OnClickListener(){
             @Override
             public void onClick(View view){
-                //TODO display dialog
+                setToRequested();
             }
         });
 
         doneButton.setOnClickListener(new Button.OnClickListener(){
             @Override
             public void onClick(View view){
-               //TODO display dialog
+               setToDone();
             }
         });
 
@@ -193,24 +193,31 @@ public class RequesterViewTaskActivity extends RequesterLayout {
             setRequestedView();
         }
         else if (status.equals("Bidded")){
+            /* displays a list of bids */
             setBiddedView();
             bidList = task.getBids();
             bidAdapter.notifyDataSetChanged();
         }
         else if (status.equals("Requested")){
+            /* displays provider info and requested/done buttons */
             setAssignedView();
             bidList = task.getBids();
             winningBid = bidList.getBid(0);
             setBidViews(winningBid.getName(), winningBid.getAmountAsString());
         }
         else if (status.equals("Done")){
+            /* displays provider info and allows a review to be left */
             setDoneView();
             bidList = task.getBids();
             winningBid = bidList.getBid(0);
             setBidViews(winningBid.getName(), winningBid.getAmountAsString());
+            //TODO allow requester to leave a review for the provider
         }
     }
 
+    /**
+     * Shows certain layout elements when the task status is Requested
+     */
     private void setRequestedView(){
         listView.setVisibility(View.GONE);
         bidderName.setVisibility(View.GONE);
@@ -222,8 +229,25 @@ public class RequesterViewTaskActivity extends RequesterLayout {
         doneButton.setVisibility(GONE);
     }
 
+    /**
+     * Shows certain layout elements when the task status is Bidded
+     */
     private void setBiddedView(){
         listView.setVisibility(GONE);
+        bidderName.setVisibility(VISIBLE);
+        bidderNameView.setVisibility(VISIBLE);
+        acceptedBid.setVisibility(VISIBLE);
+        acceptedBidView.setVisibility(VISIBLE);
+        setStatusTo.setVisibility(GONE);
+        requestedButton.setVisibility(GONE);
+        doneButton.setVisibility(GONE);
+    }
+
+    /**
+     * Shows certain layout elements when the task status is Assigned
+     */
+    private void setAssignedView(){
+        listView.setVisibility(View.GONE);
         bidderName.setVisibility(VISIBLE);
         bidderNameView.setVisibility(VISIBLE);
         acceptedBid.setVisibility(VISIBLE);
@@ -231,23 +255,28 @@ public class RequesterViewTaskActivity extends RequesterLayout {
         setStatusTo.setVisibility(VISIBLE);
         requestedButton.setVisibility(VISIBLE);
         doneButton.setVisibility(VISIBLE);
-    }
-
-    private void setAssignedView(){
-        bidderName.setVisibility(VISIBLE);
-        bidderNameView.setVisibility(VISIBLE);
-        acceptedBid.setVisibility(VISIBLE);
-        acceptedBidView.setVisibility(VISIBLE);
         //TODO leaving review button
     }
 
+    /**
+     * Shows certain layout elements when the task status is Done
+     */
     private void setDoneView(){
+        listView.setVisibility(View.GONE);
         bidderName.setVisibility(VISIBLE);
         bidderNameView.setVisibility(VISIBLE);
         acceptedBid.setVisibility(VISIBLE);
         acceptedBidView.setVisibility(VISIBLE);
+        setStatusTo.setVisibility(GONE);
+        requestedButton.setVisibility(GONE);
+        doneButton.setVisibility(GONE);
     }
 
+    /**
+     * Updates the TextViews that are conditionally displayed, depending on status.
+     * @param bidder The bidder's username
+     * @param bidAmount The winning bid's amount
+     */
     private void setBidViews(final String bidder, final String bidAmount){
         runOnUiThread(new Runnable() {
             @Override
@@ -256,6 +285,63 @@ public class RequesterViewTaskActivity extends RequesterLayout {
                 acceptedBidView.setText(bidAmount);
             }
         });
+    }
+
+    /**
+     * Handles status change to "Requested" or "Done" after the user confirms the change in a dialog.
+     * @param confirmed True if the user chose "Yes" on the dialog
+     */
+    public void onFinishConfirmDialog(Boolean confirmed){
+        if (confirmed) {
+            if (dialogFlag.equals("Requested")) {
+                task.setRequested();
+                setTaskViews();
+                setRequestedView();
+                elasticSearchController.updateTasks(task);
+            } else if (dialogFlag.equals("Done")) {
+                task.setDone();
+                setTaskViews();
+                setDoneView();
+                elasticSearchController.updateTasks(task);
+            }
+        }
+    }
+
+    /**
+     * The dialog that appears when the Set Status To: Requested button is pressed.
+     */
+    public void setToRequested(){
+        FragmentManager fm = getSupportFragmentManager();
+        dialogFlag = "Requested";
+
+        ConfirmDialog confirmDialog = new ConfirmDialog();
+        Bundle args = new Bundle();
+        args.putString("title", "Set Status To Requested");
+        args.putString("cancel", "Cancel");
+        args.putString("confirm", "Yes");
+        args.putString("message", "Are you sure you want to set the status to Requested? This will" +
+                "reopen bidding.");
+
+        confirmDialog.setArguments(args);
+        confirmDialog.show(fm, "To Requested");
+    }
+
+    /**
+     * The dialog that appears when the Set Status To: Done button is pressed.
+     */
+    public void setToDone(){
+        FragmentManager fm = getSupportFragmentManager();
+        dialogFlag = "Done";
+
+        ConfirmDialog confirmDialog = new ConfirmDialog();
+        Bundle args = new Bundle();
+        args.putString("title", "Set Status to Done");
+        args.putString("cancel", "Cancel");
+        args.putString("confirm", "Yes");
+        args.putString("message", "Are you sure you want to set status to Done?");
+
+        confirmDialog.setArguments(args);
+        confirmDialog.show(fm, "To Done");
     }
 
     /**
