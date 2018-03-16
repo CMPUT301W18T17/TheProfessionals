accd8cf7b25f9967a8bfa008c37543192b770d33
guessworks <hubertle@cs.ualberta.ca>
2018-03-16 13:15:54 -0600

    Worked on RequesterEditTaskActivityTest

--- app/src/androidTest/java/professional/team17/com/professional/RequesterEditTaskActivityTest.java
+++ app/src/androidTest/java/professional/team17/com/professional/RequesterEditTaskActivityTest.java
@@ -9,9 +9,7 @@
 package professional.team17.com.professional;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
+import android.content.Intent;
 import android.test.ActivityInstrumentationTestCase2;
 import android.widget.EditText;
 
@@ -37,23 +35,27 @@ public class RequesterEditTaskActivityTest extends ActivityInstrumentationTestCa
      * @throws Exception
      */
     public void setUp() throws Exception {
-        Solo solo = new Solo(getInstrumentation(), getActivity());
+        /* Create mock task and push to server */
         MockTask mockTask = new MockTask("test", "Test Title", "Test Description", "Test Location", "01/01/2000");
         ElasticSearchController mockES = new ElasticSearchController();
         String ID = mockES.addTasks(mockTask);
         mockTask.setId(ID);
+        /* Put the task ID in the intent */
+        /* Adapted from https://stackoverflow.com/questions/8335239/android-robotium-test-activity-that-expects-an-extra */
+        Intent i = new Intent();
+        i.putExtra("ID", mockTask.getUniqueID());
+        setActivityIntent(i);
+        /* Make solo object */
+        solo = new Solo(getInstrumentation(), getActivity());
+
     }
 
     public void testStart() {
+        /* Build activity */
         Activity activity = getActivity();
     }
 
-    /**
-     * Tear down the test.
-     */
-    public void tearDown(){
-        solo.finishOpenedActivities();
-    }
+
 
     /**
      * Tests to see what happens if the edit fields are left blank
@@ -62,7 +64,7 @@ public class RequesterEditTaskActivityTest extends ActivityInstrumentationTestCa
         /* Check that this is the right activity */
         solo.assertCurrentActivity("Wrong Activity", RequesterEditTaskActivity.class);
         /* Press the submit button without changing anything */
-        solo.clickOnButton(R.id.submitButton);
+        solo.clickOnView(solo.getView(R.id.submitButton));
         /* Check that the submit button leads to the right place */
         solo.assertCurrentActivity("Wrong activity after pressing submit", RequesterViewListActivity.class);
         solo.clickOnView(solo.getView(R.id.requester_requested_title));
@@ -76,6 +78,32 @@ public class RequesterEditTaskActivityTest extends ActivityInstrumentationTestCa
 
     public void testFillEditFields() {
         /* Check that this is the right activity */
+        solo.assertCurrentActivity("Wrong Activity", RequesterEditTaskActivity.class);
+        /*Change text fields */
+        solo.enterText((EditText) solo.getView(R.id.TaskNameField), "Test Title Changed");
+        solo.enterText((EditText) solo.getView(R.id.taskDescriptionField), "Test Description Changed");
+        solo.enterText((EditText) solo.getView(R.id.textualAddressField), "Test Location Changed");
+        solo.clickOnView(solo.getView(R.id.calendarButton));
+        /* Locate datepicker and set date */
+        solo.getCurrentActivity().getFragmentManager().findFragmentByTag("datePicker");
+        solo.setDatePicker(0, 2001, 02, 02);
+        solo.clickOnText("OK");
+        /* Check that the submit button leads to the right place */
+        solo.clickOnView(solo.getView(R.id.submitButton));
+        solo.assertCurrentActivity("Wrong activity after pressing submit", RequesterViewListActivity.class);
+        solo.clickOnView(solo.getView(R.id.requester_requested_title));
+        /* Check that clicking on the task title leads to the right place */
+        solo.assertCurrentActivity("Wrong activity after clicking on task title", RequesterViewTaskActivity.class);
+        assertTrue(solo.waitForText("Test Title Changed"));
+        assertTrue(solo.waitForText("Test Description Changed"));
+        assertTrue(solo.waitForText("Test Location Changed"));
+        assertTrue(solo.waitForText("02/02/2001"));
+    }
+    /**
+     * Tear down the test.
+     */
+    public void tearDown(){
+        solo.finishOpenedActivities();
     }
 
 }
