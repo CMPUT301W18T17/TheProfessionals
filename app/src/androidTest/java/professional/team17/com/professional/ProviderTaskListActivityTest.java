27b96b0db00d22a5cc41571cf105f2f2fee9fad0
sewingpillows <allison.boukall@gmail.com>
2018-03-19 14:15:41 -0600

    changes and log

--- app/src/androidTest/java/professional/team17/com/professional/ProviderTaskListActivityTest.java
+++ app/src/androidTest/java/professional/team17/com/professional/ProviderTaskListActivityTest.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.test.ActivityInstrumentationTestCase2;
+import android.util.Log;
 
 import com.robotium.solo.Solo;
 
@@ -17,12 +18,21 @@ public class ProviderTaskListActivityTest extends ActivityInstrumentationTestCas
     private ElasticSearchController mockES;
     private Profile mockProfile;
     private TaskList tasklist;
-    public ProviderTaskListActivityTest() {
-        super(ProviderTaskListActivity.class);
-    }
+
     private String username = "testuser";
+    private String id;
+
+
+    public ProviderTaskListActivityTest() {
+
+        super(ProviderTaskListActivity.class);
+        Log.i("START2", "testStart: ");
+
+    }
+
 
     public void setUp() throws Exception {
+        Log.i("START1", "testStart: ");
         Intent i = new Intent();
         i.putExtra("Status", "Bidded");
         setActivityIntent(i);
@@ -32,33 +42,38 @@ public class ProviderTaskListActivityTest extends ActivityInstrumentationTestCas
         editor.putString("username", "testuser"); // Storing string
         editor.commit();
         mockES = new ElasticSearchController();
-
         mockProfile = new Profile("kaixiang","TestUser1", "tester@ualberta.ca","123-456-7890");
         mockES.addProfile(mockProfile);
-        mockTask = new MockTask("kaixiang", "kaixiang's task", "Test Description", "Test Location", "01/01/2000");
+        mockTask = new MockTask("TestUser1", "kaixiang's task tasklist", "Test Description", "Test Location", "01/01/2000");
         mockTask.addBid(new Bid(username, 45.67));
-        String ID = mockES.addTasks(mockTask);
-        mockTask.setId(ID);
+        id = mockES.addTasks(mockTask);
+        Log.i("WEWE", "setUpger: "+id);
+        mockTask.setId(id);
 
         solo = new Solo(getInstrumentation(), getActivity());
     }
 
     public void testStart() throws Exception {
+
+        Log.i("START3", "testStart: ");
         Activity activity = getActivity();
+
     }
 
     @Override
     public void tearDown() throws Exception {
-        mockES.deleteTasks(mockTask);
-        mockES.deleteProfile(mockProfile);
+        Log.i("START4", "testStart: ");
         solo.finishOpenedActivities();
+        Log.i("START5", "testStart: ");
+       //mockES.deleteTasks(mockTask);
+        //mockES.deleteProfile(mockProfile);
 
     }
 
     public void testViewOtherProfile() throws Exception {
+        Log.i("START6", "testStart: ");
         tasklist = mockES.getTasksBidded(username, "Bidded");
         solo.assertCurrentActivity("Wrong Activity", ProviderTaskListActivity.class);
-
         solo.clickInList(0);
         solo.assertCurrentActivity("Wrong Activity", ProviderViewTask.class);
 
@@ -67,6 +82,7 @@ public class ProviderTaskListActivityTest extends ActivityInstrumentationTestCas
         solo.assertCurrentActivity("Wrong Activity", OtherProfileViewActivity.class);
         solo.clickOnButton("DONE");
         solo.assertCurrentActivity("Wrong  Activity", ProviderViewTask.class);
+
     }
 
 
