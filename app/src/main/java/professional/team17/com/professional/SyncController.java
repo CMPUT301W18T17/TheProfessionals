132022c66d35f4abc4f1dc3e32cd55fcfe7ffdc2
sewingpillows <allison.boukall@gmail.com>
2018-03-25 18:40:09 -0600

    updates to allow for db

---
+++ app/src/main/java/professional/team17/com/professional/SyncController.java
@@ -0,0 +1,16 @@
+package professional.team17.com.professional;
+
+/**
+ * Created by ag on 2018-03-25.
+ */
+
+public class SyncController {
+
+
+    public SyncController(){
+        // Nothing
+    }
+
+    
+
+}
