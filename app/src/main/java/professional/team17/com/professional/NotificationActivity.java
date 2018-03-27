8715bf302f8cf4ac154d18c5a0a193f7b9158095
guessworks <hubertle@cs.ualberta.ca>
2018-03-27 14:58:42 -0600

    Notification basic functionality implemented

--- app/src/main/java/professional/team17/com/professional/NotificationActivity.java
+++ app/src/main/java/professional/team17/com/professional/NotificationActivity.java
@@ -1,10 +1,12 @@
 package professional.team17.com.professional;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ListView;
@@ -16,6 +18,9 @@ public class NotificationActivity extends AppCompatActivity {
     private ImageButton backButton;
     private NotificationList notificationList;
     private String username;
+    private Profile profile;
+    private NotificationListAdapter notificationAdapter;
+    private ElasticSearchController elasticSearchController = new ElasticSearchController();
 
 
     @Override
@@ -30,6 +35,12 @@ public class NotificationActivity extends AppCompatActivity {
         SharedPreferences preferences = getSharedPreferences("MyPref", Context.MODE_PRIVATE);
         username = preferences.getString("username", "error");
 
+        /* populate notificationList */
+        getFromServer();
+        notificationAdapter = new NotificationListAdapter(this, notificationList.getList());
+        notificationListView.setAdapter(notificationAdapter);
+        notificationListView.setOnItemClickListener(clickListener);
+        notificationAdapter.notifyDataSetChanged();
 
         /* onclicklisteners */
         backButton.setOnClickListener(new View.OnClickListener() {
@@ -45,7 +56,34 @@ public class NotificationActivity extends AppCompatActivity {
                 //TODO implement clearing notifications
             }
         });
+
+
     }
 
-    //TODO implement array adapter for notification list
+    private void getFromServer(){
+        profile = elasticSearchController.getProfile(username);
+        notificationList = profile.getNotificationList();
+    }
+
+    private AdapterView.OnItemClickListener clickListener = new AdapterView.OnItemClickListener(){
+        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+            Notification notification = notificationList.get(position);
+            if (notification.getType().equals("New Bid")) {
+                Intent intent = new Intent(NotificationActivity.this, RequesterViewTaskActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("task", notification.getNotificationTaskID());
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+            else if (notification.getType().equals("Task Assigned")){
+                Intent intent = new Intent(NotificationActivity.this, ProviderViewTask.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("Task", notification.getNotificationTaskID());
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+
+        }
+
+    };
 }
---
+++ app/src/main/java/professional/team17/com/professional/NotificationListAdapter.java
@@ -0,0 +1,58 @@
+package professional.team17.com.professional;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * An adapter to show the list of notifications.
+ * @author lauren (used code from allison's BidListAdapter)
+ * @see NotificationList
+ * @see NotificationActivity
+ * @see Notification
+ */
+
+public class NotificationListAdapter extends ArrayAdapter<Notification> {
+
+
+    /**
+     * Makes a NotificationListAdapter object
+     * @param context The activity in which it's used
+     * @param notificationList The list of notifications
+     */
+    public NotificationListAdapter(Activity context, ArrayList<Notification> notificationList) {
+        super(context, 0,  notificationList);
+    }
+
+    /**
+     * Sets the row view items
+     * @param position The position of the notification in a list
+     * @param v The view object
+     * @param parent The parent viewgroup
+     * @return updated view object
+     */
+    public View getView(int position, View v, ViewGroup parent) {
+        final Notification notification= getItem(position);
+        if (v == null) {
+            v = LayoutInflater.from(getContext()).inflate(R.layout.notification_row, parent, false);
+        }
+
+        //get view
+        TextView notificationTypeField = (TextView) v.findViewById(R.id.notificationTypeField);
+        TextView notificationMessageField = (TextView) v.findViewById(R.id.notificationMessageField);
+
+
+        //plug in item to row
+        notificationTypeField.setText(notification.getType());
+        notificationMessageField.setText(notification.getMessage());
+
+
+        return v;
+    }
+}
