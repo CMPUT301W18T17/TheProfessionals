406a114b65f2e2b7c10c0058e54524d7aacc960b
Hailan Xu <hailan@ualberta.ca>
2018-03-27 23:47:09 -0600

    getScreenBoundary() in MapsSearchTasksActivity.java is done

--- app/src/main/java/professional/team17/com/professional/MapsSearchTasksActivity.java
+++ app/src/main/java/professional/team17/com/professional/MapsSearchTasksActivity.java
@@ -10,29 +10,78 @@
 
 package professional.team17.com.professional;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 
 /**
  * An activity which shows all tasks with status Requested and Bidded within 5km.
  */
 public class MapsSearchTasksActivity extends MapsActivity implements OnMapReadyCallback {
-// Calculate tasks within 4km
+// Calculate tasks within 5km
     private ImageView currentLocationButton;
+    private ImageView closeButton;
+
     public void setContentViewFunction(){
         setContentView(R.layout.activity_maps_search_tasks);
     }
     public void MapsSearchEvent(){
         currentLocationButton = (ImageView) findViewById(R.id.ic_currentlocation);
+        closeButton = (ImageView) findViewById(R.id.ic_close);
+
         currentLocationButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 getCurrentLocation();
             }
         });
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MapsSearchTasksActivity.this, SearchActivity.class));
+            }
+        });
     }
 
+    public void afterLocationFoundEvent(){
+        if (currentLatLng != null){
+            mMap.addCircle(new CircleOptions()
+                    .center(currentLatLng)
+                    .radius(5000)
+                    .fillColor(0x40ff0000)
+                    .strokeColor(Color.TRANSPARENT)
+                    .strokeWidth(2));
+        }
+        getScreenBoundary();
 
+    }
+
+    private void getScreenBoundary(){
+        LatLngBounds mapScreen = mMap.getProjection().getVisibleRegion().latLngBounds;
+        double topLeftLat = mapScreen.northeast.latitude;
+        double topLeftLon = mapScreen.southwest.longitude;
+        double bottomRightLat =mapScreen.southwest.latitude;
+        double bottomRightLon =mapScreen.northeast.longitude;
+
+        LatLng topLeft = new LatLng(topLeftLat, topLeftLon);
+        LatLng bottomRight = new LatLng(bottomRightLat, bottomRightLon);
+
+        Toast.makeText(this, "TOPLEFT: " + topLeft.toString() +
+                "BOTTOMRIGHT: " + bottomRight.toString()  , Toast.LENGTH_LONG).show();
+    }
+    /**
+     * Goes to SeachActivity.class
+     */
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(MapsSearchTasksActivity.this, SearchActivity.class));
+    }
 }
