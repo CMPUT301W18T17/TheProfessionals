63fc2a1dd7a9db3fdd5fd4369f910b4dc7bb18b7
Unknown <ltyue@ualberta.ca>
2018-03-28 18:00:55 -0600

    Reviews Attempt 2

---
+++ app/src/main/java/professional/team17/com/professional/AddReview.java
@@ -0,0 +1,13 @@
+package professional.team17.com.professional;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddReview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_review);
+    }
+}
