c711825c28eaf321ad1966098e29251b56e32fd7
Unknown <ltyue@ualberta.ca>
2018-04-01 16:01:13 -0600

    Now tracking the buttons in the activity

--- app/src/main/java/professional/team17/com/professional/AddReview.java
+++ app/src/main/java/professional/team17/com/professional/AddReview.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RatingBar;
 
@@ -23,12 +24,28 @@ public class AddReview extends AppCompatActivity {
         SharedPreferences sharedpreferences = getSharedPreferences("MyPref", Context.MODE_PRIVATE);
         reviewer = sharedpreferences.getString("username", "error");
 
+        final Button submit = findViewById(R.id.submitButton);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onSubmit(view);
+            }
+        });
+
+        final Button back = findViewById(R.id.backButton);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
     }
 
     private void onSubmit(View v){
         AddReviewController controller = new AddReviewController();
-        EditText commentBox = findViewById(R.id.commentBox);
-        RatingBar ratingBar = findViewById(R.id.ratingBar);
+        EditText commentBox = v.findViewById(R.id.commentBox);
+        RatingBar ratingBar = v.findViewById(R.id.ratingBar);
         int rating = ratingBar.getNumStars();
         String comment = commentBox.getText().toString();
 
@@ -36,8 +53,5 @@ public class AddReview extends AppCompatActivity {
 
         finish();
     }
-
-    private void back(View v){
-        finish();
-    }
+    
 }
