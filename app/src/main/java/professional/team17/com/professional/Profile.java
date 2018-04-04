e92d869832c81497ef6cec6260bd74ee9ebd55cd
Unknown <ltyue@ualberta.ca>
2018-04-04 17:42:04 -0600

    Default constructor for profile added

--- app/src/main/java/professional/team17/com/professional/Profile.java
+++ app/src/main/java/professional/team17/com/professional/Profile.java
@@ -62,6 +62,21 @@ public class Profile implements Serializable {
         notificationList = new NotificationList();
     }
 
+    public Profile(String name, String userName, String email, String phoneNumber) {
+        this.name = name;
+        this.userName = userName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.reviewList = new ReviewList();
+        this.blackList = new ArrayList<String>();
+        this.blackListedBy = new ArrayList<String>();
+        this.photoString = "-1";
+        this.config = null;
+        this.width = 0;
+        this.height = 0;
+        notificationList = new NotificationList();
+    }
+
     /**
      *
      * @return a string repr the users full name
--- app/src/main/java/professional/team17/com/professional/providerCheckImage.java
+++ app/src/main/java/professional/team17/com/professional/providerCheckImage.java
@@ -58,5 +58,4 @@ public class providerCheckImage extends AppCompatActivity {
                 .into(imageUpload);
 
     }
-
 }
