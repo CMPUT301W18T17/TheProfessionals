53b71146f6ea088ee8bbac6efed1246c286de881
Unknown <ltyue@ualberta.ca>
2018-04-04 15:05:01 -0600

    Javadocs and copyright for AddReviewController

--- app/src/main/java/professional/team17/com/professional/AddReviewController.java
+++ app/src/main/java/professional/team17/com/professional/AddReviewController.java
@@ -1,20 +1,48 @@
+/*
+ * AddReviewController
+ *
+ * April 02, 2018
+ *
+ * Copyright @ 2018 Team 17, CMPUT 301, University of Alberta - All Rights Reserved.
+ * You may use, distribute, or modify this code under terms and conditions of the Code of Student Behaviour at the University of Alberta.
+ * You can find a copy of the license in the github wiki for this project.
+ */
+
 package professional.team17.com.professional;
 
 
 import android.content.Context;
 import android.content.SharedPreferences;
 
+/**
+ * Takes the values for a review and saves it to the reviewed profile
+ *
+ * @see Review
+ * @see ReviewList
+ *
+ */
 public class AddReviewController {
 
     private Context context;
 
     private ServerHelper serverHelper;
 
+    /**
+     * Constructs an AddReviewController object
+     *
+     */
     AddReviewController(Context context){
         this.context = context;
         serverHelper = new ServerHelper(context);
     }
 
+    /**
+     * Creates a review object and attaches it to the reviewed profile.
+     *
+     * @param profileName name of the profile being reviewed
+     * @param rating rating given by the user
+     * @param comment comment provided by the user
+     */
     public void setReview(String profileName, float rating, String comment) {
         SharedPreferences sharedpreferences = context.getSharedPreferences("MyPref", Context.MODE_PRIVATE);
         String reviewer = sharedpreferences.getString("username", "error");
