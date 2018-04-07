56fdec8dff9f013c16a80685bb166a4b191e5364
Hailan Xu <hailan@ualberta.ca>
2018-04-06 19:10:52 -0600

    javadocs from rviewtaskcontroller

--- app/src/main/java/professional/team17/com/professional/RequesterViewTaskController.java
+++ app/src/main/java/professional/team17/com/professional/RequesterViewTaskController.java
@@ -15,6 +15,7 @@ import java.util.ArrayList;
 /**
  * Created by Hailan on 2018-04-05.
  * Referenced from Allison's ProviderTaskController
+ * @see RequesterViewTaskActivity
  */
 
 public class RequesterViewTaskController {
@@ -28,12 +29,21 @@ public class RequesterViewTaskController {
         serverHelper = new ServerHelper(context);
     }
 
+    /**
+     * Get old task from server by ID
+     * @param taskID
+     * @return
+     */
     public Task getOldTaskFromServer(String taskID){
         taskid = taskID;
         task = serverHelper.getTask(taskID);
         return task;
     }
 
+    /**
+     * put location info as extra (prep for MapShowALocationActivity
+     * @param intent
+     */
     public void getLocation(Intent intent) {
         Bundle bundle = new Bundle();
         bundle.putParcelable("aTaskLatLng", task.getLatLng());
@@ -41,6 +51,10 @@ public class RequesterViewTaskController {
         intent.putExtra("aTaskAddress", task.getLocation());
     }
 
+    /**
+     *
+     * @return
+     */
     public LatLng checkTaskLatLng(){
         return task.getLatLng();
     }
@@ -55,11 +69,22 @@ public class RequesterViewTaskController {
         }
     }
 
+    /**
+     * Put task photo as extra to prepare for viewing photo activity
+     * @param intent
+     */
     public void photos(Intent intent){
         ArrayList<String> photos = task.getPhotos(); // store the image in your bitmap
         intent.putStringArrayListExtra("yourImage", photos);
     }
 
+    /**
+     * Set info in task views
+     * @param titleView - TextView for title
+     * @param dateView - TextView for date
+     * @param descriptionView - TextView for description
+     * @param statusView - - TextView for status
+     */
     public void setInfoInTaskViews(TextView titleView, TextView dateView, TextView descriptionView, TextView statusView){
         titleView.setText(task.getName());
         dateView.setText(task.getDateAsString());
@@ -67,35 +92,64 @@ public class RequesterViewTaskController {
         statusView.setText(task.getStatus());
     }
 
+    /**
+     * get first item from bidlist
+     * @return first item in bidlist
+     */
     public Bid getFirstFromBidList(){
         return getCurrentTaskBidList().getBid(0);
     }
 
+
+    /**
+     * get bidlist of current task
+     * @return - list of bids
+     */
     public BidList getCurrentTaskBidList(){
         return task.getBids();
     }
 
+    /**
+     * get status of current task
+     * @return
+     */
     public String getCurrentTaskStatus(){
         return task.status;
     }
 
+    /**
+     * Hide Views
+     * @param values - Views
+     */
     public void hide(View... values) {
         for (View value : values) {
             value.setVisibility(View.GONE);
         }
     }
 
+    /**
+     * Show Views
+     * @param values - Views
+     */
     public void show(View... values) {
         for (View value : values) {
             value.setVisibility(View.VISIBLE);
         }
     }
 
+    /**
+     * Assign a bid to carry out task
+     * @param bid - the bid that the requester chose
+     */
     public void choseBid(Bid bid){
         task.chooseBid(bid);
         task.setStatus("Assigned");
     }
 
+    /**
+     * Send notification to bidder that (s)he won
+     * @param bidder - the bidder that has to carry out the task
+     */
     public void sendNotifications(String bidder){
         Profile bidderProfile = serverHelper.getProfile(bidder);
         NotificationList notificationList = bidderProfile.getNotificationList();
@@ -104,18 +158,31 @@ public class RequesterViewTaskController {
         serverHelper.addProfile(bidderProfile);
     }
 
+    /**
+     * Remove a bid from task
+     * @param bid
+     */
     public void decline(Bid bid){
         task.removeBid(bid);
     }
 
+    /**
+     * Set task status to requested
+     */
     public void setTaskToRequested(){
         task.setRequested();
     }
 
+    /**
+     * Set task status to done
+     */
     public void setTaskToDone(){
         task.setDone();
     }
 
+    /**
+     * Update the info of the task to server
+     */
     public void addOldTaskToServer(){
         serverHelper.updateTasks(task);
     }
